606f1d3727e905f1fe1167d5b609a5b7e59c5ce6
fjrdwc <fjrdwc@gmail.com>
2024-06-26 10:37:59 +0700

    add login admin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">Hello world</p>
-        <p>Test</p>
-        <p>aku pengen makan</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="px-6 md:px-0 flex md:justify-center">
+      <div className="grid border rounded-md  p-6 space-y-4 w-full md:w-1/4 mt-32">
+        <h1 className="font-semibold text-52  text-[24px]">Login admin page</h1>
+        {/* input telp */}
+        <label htmlFor="telp">
+          <p className="font-medium">Password</p>
+          <input
+            type="text"
+            placeholder="No telp"
+            className="border w-full p-2"
+          />
+        </label>
+        {/* input telp */}
+
+        {/* input password */}
+        <label htmlFor="password">
+          <p className="font-medium">Password</p>
+          <input
+            type="text"
+            placeholder="Masukkan password"
+            className="border w-full p-2"
+          />
+        </label>
+        {/* input password */}
+
+        <button className="bg-blue-400 text-white p-2 mt-[5000px]">
+          Login
+        </button>
+      </div>
     </div>
   );
 }
