d05b1a9808fd473e8ab7a0472b922328bec61231
fjrdwc <fjrdwc@gmail.com>
2024-06-26 20:11:40 +0700

    perbaiki login

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ function App() {
   }, []);
   return (
     <div className="px-6 md:px-0 flex md:justify-center">
-      <div className="grid border rounded-md  p-6 space-y-4 w-full md:w-1/4 mt-32">
-        <h1 className="font-semibold text-52  text-[24px]">Login admin page</h1>
+      <div className="grid border bg-white rounded-md  p-6 space-y-4 w-full md:w-1/2 xl:w-1/4 mt-32">
+        <h1 className="font-semibold text-52  text-[24px]">Login admin</h1>
         {/* input telp */}
         <label htmlFor="telp">
-          <p className="font-medium">Password</p>
+          <p className="font-medium">No telepon</p>
           <input
             type="text"
-            placeholder="No telp"
-            className="border w-full p-2"
+            placeholder="Masukkan no telepon"
+            className="border  bg-white  w-full p-2"
           />
         </label>
         {/* input telp */}
 
         {/* input password */}
-        <label htmlFor="password">
+        <label htmlFor="password" className="mb-24">
           <p className="font-medium">Password</p>
           <input
-            type="text"
+            type="password"
             placeholder="Masukkan password"
-            className="border w-full p-2"
+            className="border bg-white w-full p-2 "
           />
         </label>
         {/* input password */}
