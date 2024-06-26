c15568b5eec4091afda8616268c9634b3fefdb3e
fjrdwc <fjrdwc@gmail.com>
2024-06-26 09:42:06 +0700

    install tailwind css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world</p>
+        <p className="text-red-500">Hello world</p>
         <p>Test</p>
         <p>aku pengen makan</p>
         <a
