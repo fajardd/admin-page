1d81b6154194a6310dd984cd147628f4cb6c5a1c
fjrdwc <fjrdwc@gmail.com>
2024-07-13 19:43:54 +0700

    update TableRowCustomer

--- src/components/Fragments/TableRowCustomer.js
+++ src/components/Fragments/TableRowCustomer.js
@@ -5,7 +5,7 @@ function TableRowCustomer({ customer, index, currentPage }) {
     <tr key={customer.id_user} className="bg-white border-b">
       <td className="px-6 py-4">{(currentPage - 1) * 5 + index + 1}</td>
       <td className="px-6 py-4">{customer.nama}</td>
-      <td className="px-6 py-4">{customer.role.role_name}</td>
+      <td className="px-6 py-4">{customer.role?.role_name || "N/A"}</td>
       <td className="px-6 py-4">{customer.no_telp}</td>
       <td className="px-6 py-4">{customer.email}</td>
       <td className="px-6 py-4">Edit || Delete</td>
