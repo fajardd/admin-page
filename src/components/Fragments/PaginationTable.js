59b43a4d791b8685b500523e6a60361bd5ec1a76
fjrdwc <fjrdwc@gmail.com>
2024-07-17 12:40:57 +0700

    update pigination table

--- src/components/Fragments/PaginationTable.js
+++ src/components/Fragments/PaginationTable.js
@@ -3,18 +3,49 @@ import React from "react";
 import ButtonPagination from "../Elements/ButtonPagination";
 
 const PaginationTable = ({ currentPage, totalPages, handlePageChange }) => {
+  const getPageNumbers = () => {
+    const pageNumbers = [];
+    if (totalPages <= 4) {
+      for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+      }
+    } else {
+      if (currentPage <= 4) {
+        pageNumbers.push(1, 2, 3, 4, 5, "...", totalPages);
+      } else if (currentPage >= totalPages - 3) {
+        pageNumbers.push(
+          1,
+          "...",
+          totalPages - 4,
+          totalPages - 3,
+          totalPages - 2,
+          totalPages - 1,
+          totalPages
+        );
+      } else {
+        pageNumbers.push(
+          1,
+          "...",
+          currentPage - 1,
+          currentPage,
+          currentPage + 1,
+          "...",
+          totalPages
+        );
+      }
+    }
+    return pageNumbers;
+  };
+
+  const pageNumbers = getPageNumbers();
   return (
     <nav
       className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
       aria-label="Table navigation"
     >
       <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-        Showing{" "}
-        <span className="font-semibold text-graya-400 ">
-          {(currentPage - 1) * 5 + 1}-{currentPage * 5}
-        </span>{" "}
-        of{" "}
-        <span className="font-semibold text-gray-400 ">{totalPages * 5}</span>
+        Showing {(currentPage - 1) * 5 + 1}-{currentPage * 5} of{" "}
+        {totalPages * 5}
       </span>
       <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
         <li>
@@ -26,18 +57,24 @@ const PaginationTable = ({ currentPage, totalPages, handlePageChange }) => {
             Previous
           </ButtonPagination>
         </li>
-        {[...Array(totalPages).keys()].map((page) => (
-          <li key={page}>
-            <ButtonPagination
-              onClick={() => handlePageChange(page + 1)}
-              className={`flex items-center justify-center px-3 h-8 leading-tight ${
-                currentPage === page + 1
-                  ? "text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
-                  : "text-gray-500 hover:text-white bg-white hover:bg-blue-500 border"
-              }`}
-            >
-              {page + 1}
-            </ButtonPagination>
+        {pageNumbers.map((page, index) => (
+          <li key={index}>
+            {typeof page === "number" ? (
+              <ButtonPagination
+                onClick={() => handlePageChange(page)}
+                className={`flex items-center justify-center px-3 h-8 leading-tight ${
+                  currentPage === page
+                    ? "text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
+                    : "text-gray-500 hover:text-white bg-white hover:bg-blue-500 border"
+                }`}
+              >
+                {page}
+              </ButtonPagination>
+            ) : (
+              <span className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500">
+                {page}
+              </span>
+            )}
           </li>
         ))}
         <li>
